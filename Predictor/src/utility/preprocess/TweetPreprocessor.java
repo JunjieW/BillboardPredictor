f06a3bc938558b082c1cdf28f9be53ba5423d9f6
JunjieW <junjie.wei@dreamcorps.org>
2015-12-14 00:35:28 +0800

    add "I uploaded a YouTube video" as a spam strings

--- Predictor/src/utility/preprocess/TweetPreprocessor.java
+++ Predictor/src/utility/preprocess/TweetPreprocessor.java
@@ -1,6 +1,7 @@
 package utility.preprocess;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,6 +9,11 @@ import java.util.regex.Pattern;
  * Created by JunjieW on 2015/12/13.
  */
 public class TweetPreprocessor {
+    private static String[] spamStrings = {
+        "Now Playing", "Check this out", "I just voted", "I favorited a YouTube video", "Check this video out",
+        "Lyrics included", "Listening to", "I uploaded a YouTube video"
+    };
+
     public static void combineDataCrossMonth(String folderOfPreviousEnd, String folderOfLaterBegin) {
         try {
             File[] previousEnd = new File(folderOfPreviousEnd).listFiles();
@@ -41,29 +47,87 @@ public class TweetPreprocessor {
                 System.out.println(previousEnd[i].getName() + "has " + count + " mentions" );
             }
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
     // http://stackoverflow.com/a/12950893
-    public static String removeUrl(String commentstr) {
-        String urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
-        Pattern p = Pattern.compile(urlPattern,Pattern.CASE_INSENSITIVE);
-        Matcher m = p.matcher(commentstr);
-        int i = 0;
-        while (m.find()) {
-            commentstr = commentstr.replaceAll(m.group(i),"").trim();
-            i++;
+    private static String removeUrl(String str) {
+        //System.out.println(str);
+        // TODO: characters that is not utf-8 has weird gone
+        String str_urls_free = str.replaceAll("([\\s*]http.*?[\\s*])", " ").replaceAll("([\\s*]http.*)", " ").replaceAll("(http.*?[\\s*])", "");
+        //System.out.println(str_urls_free);
+        return str_urls_free + ".\n";
+    }
+
+    private static boolean isSpam(String line) {
+        boolean result = false;
+        for (String spamStr : TweetPreprocessor.spamStrings) {
+            if (Pattern.compile(Pattern.quote(spamStr), Pattern.CASE_INSENSITIVE).matcher(line).find()) {
+                result = true;
+                break;
+            }
         }
-        return commentstr;
+        return result;
+    }
+
+    public static void batchRemoveUrl() {
+        File[] folders = new File("./Mentions").listFiles();
+        for (File folder: folders) {
+            String folderPath = "./Mentions/" + folder.getName();
+            try {
+                if ( new File(folderPath).isDirectory() == false ){
+                    continue;
+                }
+                System.out.println("Parsing folder ./Mentions/" + folder.getName() + " to remove urls...");
+                File[] originalFiles = new File(folderPath).listFiles();
+
+                BufferedReader in = null;
+                BufferedWriter out = null;
+                String line = "";
+                for (int i = 0; i < originalFiles.length; i++) {
+                    // System.out.println("    Parsing " + originalFiles[i].getName());
+
+                    String str_outFolderPath = folderPath + "/url_free/" ;
+                    File outFolder = new File(str_outFolderPath);
+                    if ( !outFolder.exists())
+                        outFolder.mkdir();
+                    out = new BufferedWriter(new FileWriter(str_outFolderPath + originalFiles[i].getName() + "_urls_free"));
+                    if (originalFiles[i].getName().contains("url_free")) {
+                        continue;
+                    } else {
+                        in = new BufferedReader(new FileReader(originalFiles[i]));
+                    }
+
+                    int count = 0;
+                    line = in.readLine();
+                    while (line != null) {
+                        if ( TweetPreprocessor.isSpam(line) != true) {
+                            count++;
+                            line = TweetPreprocessor.removeUrl(line);
+                            out.append(line);
+                        }
+                        line = in.readLine();
+                    }
+                    out.flush();
+
+                    out.close();
+                    in.close();
+                    System.out.println("    " + originalFiles[i].getName() + " has " + count + " non-spam mentions");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        String previous = "./2009-06-end-07-05-result";
-        String later = "./2009-07-05-result";
-        TweetPreprocessor.combineDataCrossMonth(previous,later);
+//        // Combine cross week data
+//        String previous = "./2009-06-end-07-05-result";
+//        String later = "./2009-07-05-result";
+//        TweetPreprocessor.combineDataCrossMonth(previous,later);
 
-
+        // Spam removal
+        TweetPreprocessor.batchRemoveUrl();
     }
 }
