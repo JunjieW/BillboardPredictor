55baf32df1161340a1c1cf694a1a5917c5b707a1
JunjieW <junjie.wei@dreamcorps.org>
2015-12-18 13:09:36 +0800

    Complete sentimental analysis

--- Predictor/src/utility/preprocess/TweetPreprocessor.java
+++ Predictor/src/utility/preprocess/TweetPreprocessor.java
@@ -1,8 +1,6 @@
 package utility.preprocess;
 
 import java.io.*;
-import java.nio.charset.Charset;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -51,15 +49,6 @@ public class TweetPreprocessor {
         }
     }
 
-    // http://stackoverflow.com/a/12950893
-    private static String removeUrl(String str) {
-        //System.out.println(str);
-        // TODO: characters that is not utf-8 has weird gone
-        String str_urls_free = str.replaceAll("([\\s*]http.*?[\\s*])", " ").replaceAll("([\\s*]http.*)", " ").replaceAll("(http.*?[\\s*])", "");
-        //System.out.println(str_urls_free);
-        return str_urls_free + ".\n";
-    }
-
     private static boolean isSpam(String line) {
         boolean result = false;
         for (String spamStr : TweetPreprocessor.spamStrings) {
@@ -70,8 +59,43 @@ public class TweetPreprocessor {
         }
         return result;
     }
+    // http://stackoverflow.com/a/12950893
+    private static String removeUrl(String str) {
+        //System.out.println(str);
+        // TODO: characters that is not utf-8 has weird gone
+        String str_urls_free = str.replaceAll("([\\s*]http.*?[\\s*])", " ").replaceAll("([\\s*]http.*)", " ").replaceAll("(http.*?[\\s*])", "");
+        //System.out.println(str_urls_free);
+        return str_urls_free + ".\n";
+    }
 
-    public static void batchRemoveUrl() {
+    private static String removeTitle(String str, String title) {
+        Pattern fooPattern = Pattern.compile("(?i)" + title);
+        String str_title_free = fooPattern.matcher(str).replaceAll("THE SONG");
+        return str_title_free;
+    }
+
+    private static String removeSpecialChars(String str) {
+        //String exclusive_str = "^[\\w \\d \\s \\. \\& \\+ \\- \\, \\! \\@ \\# \\$ \\% \\^ \\* \\( \\) \\; \\ \\/ \\| \\< \\> \" \\' \\? \\= \\: \\[ \\] ]";
+        // http://stackoverflow.com/a/14361671
+        // http://stackoverflow.com/a/6204599
+        String exclusive_str = "[^a-zA-Z0-9\\d\\s\\.\\&\\+\\-\\,\\!\\@\\#\\$\\%\\'\\*\\(\\)\\;\\\\/\\<\\>\"\\?\\=\\;\\:\\[\\]]";
+        String str_delimeter_free = str.replaceAll(exclusive_str, "");
+        return str_delimeter_free;
+    }
+
+    private static String removeSentenceDelimeters(String str) {
+
+        String str_delimeter_free = str.replaceAll("\\.", " ").replaceAll("!", " ").replaceAll("\\?", " ");
+        return str_delimeter_free;
+    }
+
+
+    private static String getTitle(String fileName) {
+        String title = fileName.substring(fileName.indexOf("_")+1).replaceAll("_", " ");
+        return title;
+    }
+
+    public static void batchRemoveSpam() {
         File[] folders = new File("./Mentions").listFiles();
         for (File folder: folders) {
             String folderPath = "./Mentions/" + folder.getName();
@@ -100,13 +124,41 @@ public class TweetPreprocessor {
                     }
 
                     int count = 0;
+//                    int posCount = 0;
+//                    int negCount = 0;
+//                    int neuCount = 0;
+                    String title = getTitle(originalFiles[i].getName());
                     line = in.readLine();
                     while (line != null) {
                         if ( TweetPreprocessor.isSpam(line) != true) {
                             count++;
                             line = TweetPreprocessor.removeUrl(line);
-                            out.append(line);
+                            line = TweetPreprocessor.removeTitle(line, title);
+                            line = TweetPreprocessor.removeSpecialChars(line);
+                            line = TweetPreprocessor.removeSentenceDelimeters(line);
+                            line = line.trim();
+                            out.append(line + ".\n");
+                            //System.out.println(line);
+//                            switch (SentimentAnalyst.evaluateSentiment(line)) {
+//                                case 1:
+//                                    negCount++;
+//                                    break;
+//                                case 0:
+//                                    neuCount++;
+//                                    break;
+//                                case -1:
+//                                    negCount++;
+//                                    break;
+//                            }
+//                            if ( -1 == SentimentAnalyst.evaluateSentiment( line)) {
+//                                negCount++;
+//                            } else if ( 0 == SentimentAnalyst.evaluateSentiment( line)) {
+//                                neuCount++;
+//                            } if ( 1 == SentimentAnalyst.evaluateSentiment( line)) {
+//                                posCount++;
+//                            }
                         }
+
                         line = in.readLine();
                     }
                     out.flush();
@@ -114,6 +166,8 @@ public class TweetPreprocessor {
                     out.close();
                     in.close();
                     System.out.println("    " + originalFiles[i].getName() + " has " + count + " non-spam mentions");
+//                    System.out.println("    " + originalFiles[i].getName() + " has " + count + " non-spam mentions"
+//                            + ", posCount=" + posCount+ ", negCount=" + negCount + ", neuCount=" + neuCount);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -128,6 +182,10 @@ public class TweetPreprocessor {
 //        TweetPreprocessor.combineDataCrossMonth(previous,later);
 
         // Spam removal
-        TweetPreprocessor.batchRemoveUrl();
+        TweetPreprocessor.batchRemoveSpam();
+
+        // System.out.println(TweetPreprocessor.removeTitle("hahaha BooM BOOM POW", "Boom bOom POW"));
+        //System.out.println(TweetPreprocessor.getTitle("2_I_Know_You_Want_Me_(Calle_Ocho)"));
+        //System.out.println(TweetPreprocessor.removeSpecialChars("Ultra Records and Pitbull Achieve Platinum With Radio Smash Hit â€œI Know You Want Me (Calle Ocho)â€? Mu.. ."));
     }
 }
